b0341e560392c712fe3e755fe6d2753e3b8df8a6
Anish Pithakote <muttsang@gmail.com>
2020-10-04 17:31:19 +0100

    push again?

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    float speed = 5;
+    Vector2 _movementInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(_movementInput.x, _movementInput.y)
+                            *speed
+                            * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _movementInput = ctx.ReadValue<Vector2>();
+    }
+}
