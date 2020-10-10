fb3ded928f489570a0ad0de414bbae712eba933e
Anish Pithakote <muttsang@gmail.com>
2020-10-11 00:54:38 +0100

    Made it work
    MainMenuEveny system: Changed to default. Playerconfig: Made another script that just destroys the PlayerConfigManager

---
+++ Assets/Scripts/DestroyPlayerConfigManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayerConfigManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(GameObject.Find("PlayerConfigurationManager(Clone)"));
+    }
+
+
+}
