aa9fb2c883ca5770e23fcea5ddd94d695b0b9a97
Anish Pithakote <muttsang@gmail.com>
2020-10-13 01:16:33 +0100

    Fixed all

---
+++ Assets/Scripts/DestroyPlayerConfigManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayerConfigManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   if (GameObject.Find("PlayerConfigurationManager(Clone)"))
+            Destroy(GameObject.Find("PlayerConfigurationManager(Clone)"));
+    }
+
+    
+}
